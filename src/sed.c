5ac87218441f82fe6eeb37f9d7a0108e6f9a8844
Yamada Yohei <yamadayohei@gmail.com>
2016-05-08 18:50:20 +0900

    sed: refactoring address routine

--- src/sed.c
+++ src/sed.c
@@ -3,27 +3,45 @@
 #define BUFSIZE 0x80
 int nr;
 
-void
-sub2(char *buf, char *cmd)
+int
+num_read(char *s)
 {
    int n = 0;
 
+   for (; *s >= '0' && *s <= '9'; s++)
+      n = n * 10 + (*s - '0');
+
+   return n;
+}
+
+char *
+num_skip(char *s)
+{
+   for (; *s >= '0' && *s <= '9'; s++) ;
+
+   return s;
+}
+
+void
+sub2(char *buf, char *cmd)
+{
    if (*cmd == '/')
       for (cmd++; *cmd != '/'; cmd++)
          ;
-   else if (*cmd >= '1' && *cmd <= '9') do {
-      for (; *cmd >= '0' && *cmd <= '9'; cmd++)
-         n = n * 10 + *cmd - '0';
+   else if (*cmd >= '1' && *cmd <= '9') {
+      int n0, n1;
+      n0 = num_read(cmd);
+      cmd = num_skip(cmd);
+
       if (*cmd == ',') {
          cmd++;
-         if (n > nr) return; }
-      else {
-         if (n != nr) return; else break; }
+         n1 = num_read(cmd);
+         cmd = num_skip(cmd); }
+      else
+         n1 = n0;
 
-      n = 0;
-      for (; *cmd >= '0' && *cmd <= '9'; cmd++)
-         n = n * 10 + *cmd - '0';
-      if (n < nr) return; } while (0);
+      if (n0 > nr) return;
+      if (n1 < nr) return; }
 
    switch (*cmd) {
    case 'd':
